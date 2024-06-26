dc6524aa0d7aab11f712c0ba300fa4f75dd82b8c
John Uel Mangaba <johnuelmangaba@gmail.com>
2024-06-27 02:14:22 +0800

    add some metadata and icon

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Raleway } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const raleway = Raleway({
@@ -8,12 +9,17 @@ const raleway = Raleway({
 
 export const metadata = {
   title: "Quiz App",
-  description: "Test your knowledge with our quiz app",
+  description: "Test your knowledge with our quiz app!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={raleway.className}>{children}</body>
     </html>
   );
